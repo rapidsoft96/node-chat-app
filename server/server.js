4197daeb3a781b6cbbf0afedbfe37d9542cc2f9d
rapidsoft96 <zlatkonrt1915@hotmail.com>
2018-05-10 10:41:52 +0200

    first commit in chat app

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path=require('path'); //built in module
+const express=require('express');
+const port=process.env.PORT || 3000;
+const publicPath= path.join(__dirname, '../public')
+//join - sluzi za spajanje putanje , kako bi iz servera lako dosli u public, zbog middleware-a koji je komplikovaniji
+
+var app=express();
+
+app.use(express.static(publicPath));
+
+app.listen(3000,()=>{
+  console.log(`Server is running on port ${port}`);
+});
