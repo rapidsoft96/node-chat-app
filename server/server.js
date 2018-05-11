23f67f5bf2009d0b0db790a60d82737744af0cef
rapidsoft96 <zlatkonrt1915@hotmail.com>
2018-05-11 08:49:33 +0200

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,13 +1,23 @@
 const path=require('path'); //built in module
 const express=require('express');
+const socketIO=require('socket.io');
+const http = require('http');
 const port=process.env.PORT || 3000;
 const publicPath= path.join(__dirname, '../public')
 //join - sluzi za spajanje putanje , kako bi iz servera lako dosli u public, zbog middleware-a koji je komplikovaniji
 
 var app=express();
+var server=http.createServer(app);
+var io=socketIO(server); //communicating
+
 
 app.use(express.static(publicPath));
-
-app.listen(port,()=>{
+io.on('connection',(socket)=>{ //individual socket, client
+  console.log('New user connected');
+  socket.on('disconnect',()=>{
+    console.log('Client disconnected');
+  })
+});
+server.listen(port,()=>{
   console.log(`Server is running on port ${port}`);
 });
